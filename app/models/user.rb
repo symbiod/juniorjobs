f01cd72cedd599c1c6fa07447ad9e860ecdc879d
Marina Belova <belova.ma.ev@gmail.com>
2017-12-27 23:45:02 +0300

    Add roles to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  VALID_ROLES = %w(junior company admin)
+  EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
+
+  validates :password, confirmation: true, presence: true,
+            length: { minimum: 6 }
+  validates :email, uniqueness: true, presence: true,
+            format: { with: EMAIL_REGEX }
+  validate :validate_roles
+
+  private
+
+  def validate_roles
+    invalid_roles = roles - VALID_ROLES
+    invalid_roles.each do |role|
+      errors.add(:roles, "#{role} - is not a valid role")
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171227085030) do
+ActiveRecord::Schema.define(version: 20171227173831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171227085030) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
