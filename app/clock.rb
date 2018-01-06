0c16b64016910487a511698c92c6bab2c463f51a
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-01-06 22:21:28 +0300

    Fix sidekiq initialization

--- app/clock.rb
+++ app/clock.rb
@@ -1,6 +1,7 @@
 require 'clockwork'
 require_relative "../config/boot"
 require_relative "../config/environment"
+require_relative '../config/sidekiq'
 
 module Clockwork
   every(1.day, 'sending_emails.job') { EmailDispatchesWorker.perform_async }
