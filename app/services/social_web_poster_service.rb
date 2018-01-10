4a85ba54857b82eacd81db354aac32289de66288
zero75 <master4842@yandex.ru>
2018-01-10 10:11:07 +0300

    add telegram

--- app/services/social_web_poster_service.rb
+++ app/services/social_web_poster_service.rb
@@ -1,1 +1,8 @@
 class SocialWebsPoster
+
+  def post_job(job_id)
+  VkWorker.perform_async(job_id)
+  FbWorker.perform_async(job_id)
+  TelegramWorker.perform_async(job_id)
+  end
+end
--- app/workers/fb_worker.rb
+++ app/workers/fb_worker.rb
@@ -2,15 +2,17 @@ class FbWorker
   include Sidekiq::Worker
 
   def perform(job_id)
-    job = Job.find_by(id: job_id)
-    token = ENV["FB_TOKEN_#{job.country}"]
-    target_id = ENV["FB_GROUP_ID_#{job.country}"]
-    @graph = Koala::Facebook::API.new(token)
-    @graph.put_wall_post("#{job.title} \n
-    Ссылка: http://juniorjobs.ru/job/#{job.id}",
+    set_domain(job_id)
+    @graph = Koala::Facebook::API.new(@token)
+    @graph.put_wall_post("#{@job.title} \n
+    Ссылка: http://juniorjobs.ru/job/#{@job.id}",
                          {
-                         }, target_id)
-
-   #  attachments: "http://juniorjobs.ru/job/#{job.id}")
+                         }, @target_id)
  end
+
+  def set_domain(job_id)
+    @job = Job.find_by(id: job_id)
+    @token = ENV["FB_TOKEN_#{@job.country}"]
+    @target_id = ENV["FB_GROUP_ID_#{@job.country}"]
+  end
 end
