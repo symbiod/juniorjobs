caacc22d23870b68704dad16c9f2fea650cf7986
Marina Belova <belova.ma.ev@gmail.com>
2018-01-15 18:38:00 +0300

    Add docs, fix user association order

--- app/models/concerns/has_role.rb
+++ app/models/concerns/has_role.rb
@@ -1,3 +1,5 @@
+# This module adds roles to model (junior, company, admin by default), validates present of roles and their validity.
+
 module HasRole
   extend ActiveSupport::Concern
 
@@ -8,16 +10,22 @@ module HasRole
     validate :roles_value, if: :roles_present?
   end
 
+# Adds roles to model if role is valid and doesn't already present
+# @param values [Array<String, Symbol>]
   def add_roles(*values)
     values.map(&:to_s).each do |value|
       roles << value if VALID_ROLES.include?(value) && !has_role?(value)
     end
   end
 
+# Remove roles from model
+# @param values [Array<String, Symbol>]
   def remove_roles(*values)
     self.roles = roles - values.map(&:to_s)
   end
 
+# Check if model has role
+# @param value [#to_s]
   def has_role?(value)
     roles.include?(value.to_s)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   include HasRole
-  has_many :authentications, dependent: :destroy
-  accepts_nested_attributes_for :authentications
 
-  VALID_ROLES = %w[junior company admin].freeze
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
 
+  has_many :authentications, dependent: :destroy
   has_many :jobs
   has_one :subscription
+
+  accepts_nested_attributes_for :authentications
 
   validates :password, confirmation: true, presence: true, length: { minimum: 6 }
   validates :email, uniqueness: true, presence: true, format: { with: EMAIL_REGEX }
