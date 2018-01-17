b083ddd2d949b16eb3cb5762a33dea6a34b633bc
ValentinAndreev <valentinandreev80@gmail.com>
2018-01-17 08:39:19 +0300

    Workers fixes

--- app/jobs/email_to_admin_job.rb
+++ app/jobs/email_to_admin_job.rb
@@ -2,6 +2,6 @@ class EmailToAdminJob < ApplicationJob
   queue_as :default
 
   def perform(id)
-    AdminJobMailer.delay.edit_job_email(id).deliver
+    AdminJobMailer.edit_job_email(id).deliver_later
   end
 end
--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -5,5 +5,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/1" }
 end
-
-Sidekiq::Extensions.enable_delay!
