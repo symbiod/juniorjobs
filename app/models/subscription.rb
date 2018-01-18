6878828a0686b3f04b842cc7400bcb5b47195c4d
ValentinAndreev <valentinandreev80@gmail.com>
2018-01-18 10:35:49 +0300

    Constants to settings

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   validates :email, presence: true,
-                    format: { with: EMAIL_REGEX, on: :create },
+                    format: { with: %r[#{Settings.email_regex}], on: :create },
                     uniqueness: { case_sensitive: false }
   belongs_to :user, optional: true
 end
