ec58cb52d5af160062eea1ab836705054eb48bf6
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-01-19 14:39:18 +0300

    fixup! fixup! fixup! fixup! Fix rubocops

--- app/services/create/create_subscription.rb
+++ app/services/create/create_subscription.rb
@@ -5,7 +5,7 @@
 class CreateSubscription
   def call(current_user, params)
     subscription = current_user ? current_user.build_subscription(params) : Subscription.new(params)
-    EmailDispatchesWorker.perform_async(subscription_params[:email]) if subscription.valid?
+    EmailDispatchesWorker.perform_async(params[:email]) if subscription.valid?
     subscription
   end
 end
