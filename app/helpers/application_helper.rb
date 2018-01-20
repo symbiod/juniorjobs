f8b0dca002ae6bc22bef8408b5a98f755fdc2d14
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-01-20 17:18:41 +0300

    in helper expires_at: fix strftime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,9 +42,9 @@ module ApplicationHelper
   def expires_at(job, modifier)
     num, period = modifier.split('_')
     num = num.to_i
-    text = t("activerecord.attributes.job.expired_ats.#{modifier}", date: expired_date_for(job, num.send(period)))
-           .strftime('%d/%m/%Y')
-    value = expired_date_for(job, num.send(:period))
+    text = t("activerecord.attributes.job.expired_ats.#{modifier}", 
+      date: expired_date_for(job, num.send(period)).strftime('%d/%m/%Y'))
+    value = expired_date_for(job, num.send(period))
     [text, value]
   end
 
