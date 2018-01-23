a12fb4b25c9ea629e1c245628c9c13f0f453c4d0
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-01-23 16:17:05 +0300

    render list jobs by tag

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -5,10 +5,21 @@ module Web
   # We should consider addig some documentation here
   class WelcomeController < BaseController
     def index
+      if params[:tag]
+        @tag = params[:tag]
+        @jobs_with_tag = Job.tagged_with(params[:tag])
+        
+        render :jobs_with_tag
+      end     
+                  
       @jobs_published_today = Job.published_today
       @jobs_published_yesterday = Job.published_yesterday
       @jobs_published_last_week = Job.published_last_week
       @jobs_published_last_month = Job.published_last_month
+    end
+
+    def method_name
+      
     end
   end
 end
