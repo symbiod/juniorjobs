4b278e7d087611c746b4f1d7b59a7ef579204530
Anton <toshidono.it.work@gmail.com>
2018-01-28 01:29:19 +0300

    Update Vacancy factory

--- cucumber_test/features/support/factories/vacancy.rb
+++ cucumber_test/features/support/factories/vacancy.rb
@@ -3,14 +3,21 @@
 FactoryBot.define do
   factory :vacancy, class: OpenStruct do
     Faker::Config.locale = :en
-    title Faker::Name.first_name
-    description Faker::Name.last_name
-    employment Faker::Name.last_name
-    requirements Faker::Name.last_name
-    salary_from rand(1000)
-    salary_to 500
+    title Faker::Job.title
+    employment { ['full', 'part', 'contract', 'internal'][rand(0..3)] }
+    description Faker::Lorem.paragraph
+    requirements { res = []; 10.times { res << Faker::ProgrammingLanguage.name }; res }
+    city Faker::Address.city
+    remote Faker::Boolean.boolean
     company_name Faker::Company.name
+    company_page Faker::Internet.url
     company_contact Faker::Company.swedish_organisation_number
-    company_email "nn24086+#{Time.now.to_i}@gmail.com"
+    company_phone Faker::PhoneNumber.cell_phone
+    company_email Faker::Internet.email
+    currency { ['byn', 'rub', 'uah', 'usd'][rand(0..3)] }
+    salary_from { rand(1000) }
+    salary_to { rand (1001..2000) }
+    salary_by_agreement Faker::Boolean.boolean
+    expired_at { [(Time.now + 24 * 6 * 360), (Time.now + 24 * 13 * 360)][rand(0..1)] }
   end
 end
