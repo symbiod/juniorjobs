62e72571dca095ee623c1c42d33e90aa883b56ee
pavlick <pasha1994_@mail.ru>
2018-01-29 23:46:20 +0300

    PR fix, locales, styles + decorator

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
     get '/developers', to: 'developer_cvs#index'
 
     resources :tags, only: [:index]
-    get '/cvs', to: 'cvs_public#index'
+    get '/developers', to: 'developer_cvs#index'
+
     resources :jobs
     resources :developer_cvs, only: [:index, :show], as: 'developers'
     resource :subscription, only: [:new, :create, :destroy]
