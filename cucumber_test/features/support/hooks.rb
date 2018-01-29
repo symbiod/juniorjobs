2bc3535366a8ac70940d0d1edeeb9e72ac8ec1b6
Dmitrii <Andrienko.dmitrii@gmail.com>
2018-01-29 17:54:38 +0200

    add screenshot on fail scenario

--- cucumber_test/features/support/hooks.rb
+++ cucumber_test/features/support/hooks.rb
@@ -13,7 +13,13 @@ Before do |scenario|
                                 switches: browser_options)
 end
 
-After do
+After do |scenario|
+  if scenario.failed?
+    screenshot = "./FAILED_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
+    @browser.driver.save_screenshot(screenshot)
+    encoded_img = @browser.driver.screenshot_as(:base64)
+    embed("data:image/png;base64,#{encoded_img}",'image/png')
+  end
   @browser.close
   DatabaseCleaner[:activerecord].clean
 end
