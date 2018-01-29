39a470f516579c1daa30c72a0ed2f417ce6f4574
Dmitrii <Andrienko.dmitrii@gmail.com>
2018-01-29 17:59:45 +0200

    hund fix

--- cucumber_test/features/support/hooks.rb
+++ cucumber_test/features/support/hooks.rb
@@ -15,10 +15,10 @@ end
 
 After do |scenario|
   if scenario.failed?
-    screenshot = "./FAILED_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
+    screenshot = "./FAILED_#{scenario.name.tr(' ', '_').gsub(/[^0-9A-Za-z_]/, '')}.png"
     @browser.driver.save_screenshot(screenshot)
     encoded_img = @browser.driver.screenshot_as(:base64)
-    embed("data:image/png;base64,#{encoded_img}",'image/png')
+    embed("data:image/png;base64,#{encoded_img}", 'image/png')
   end
   @browser.close
   DatabaseCleaner[:activerecord].clean
