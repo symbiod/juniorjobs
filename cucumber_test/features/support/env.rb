c1dc46a6e7b1ff1f397fad0ffa13fa505a52a89f
Stanislav Mekhonoshin <ejabberd@gmail.com>
2018-01-30 00:12:41 +0300

    Fix conflict

---
+++ cucumber_test/features/support/env.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+
+require 'rspec'
+require 'watir'
+require 'page-object'
+require 'data_magic'
+require 'factory_bot'
+require 'database_cleaner'
+
+include NavigationHelpers
+
+World(PageObject::PageFactory)
+
+Dir['./features/support/helpers/*.rb'].each { |file| require file }
