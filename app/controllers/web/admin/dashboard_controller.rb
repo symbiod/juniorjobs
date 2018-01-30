fad4ef4635a81c2bcb305852dc7a2a1cdca37883
Marina Belova <belova.ma.ev@gmail.com>
2018-01-30 18:28:16 +0300

    Add admin dashboard (#134)

---
+++ app/controllers/web/admin/dashboard_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# admin dashboard
+module Web
+  module Admin
+    class DashboardController < Web::Admin::BaseController
+      def index; end
+    end
+  end
+end
