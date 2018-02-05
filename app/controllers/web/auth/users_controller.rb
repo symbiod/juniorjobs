6e43d011b9625c297920a7f724a4a0e84e29d41f
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-02-05 15:58:12 +0300

    collection_radio_buttons param multiple true

--- app/controllers/web/auth/users_controller.rb
+++ app/controllers/web/auth/users_controller.rb
@@ -7,7 +7,6 @@ module Web
     class UsersController < Web::Auth::BaseController
       before_action :load_user, only: %i[edit update destroy]
       before_action :load_roles, only: %i[new edit create update]
-      before_action :set_roles_params_to_arr, only: %i[create] 
       before_action :require_login, :authorize_user, only: %i[edit update destroy]
 
       def new
@@ -84,10 +83,6 @@ module Web
         UserSchema.with(valid_roles: @roles).call(user_params)
       end
 
-      def set_roles_params_to_arr
-        roles = Array.new(1, params[:user][:roles])
-        params[:user][:roles] = roles
-      end
       # TODO: Use interactors
     end
   end
